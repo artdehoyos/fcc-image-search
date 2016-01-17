5dcd66bd893196058a472b500d32c598dc6ca78e
artdehoyos <adehoyos@gmail.com>
2016-01-17 13:23:53 +0000

    Added index, completed README. Can view recent searches.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # fcc-image-search
 An interface to the Google Custom Search API. Returns image links and metadata as JSON.
+
+## Example uses
+
+### Search
+Pass your request to the /search endpoint to search. You may add an offset to
+get further results. Each page is an offset of a multiple of 10.
+
+    https://cryptic-castle-1280.herokuapp.com/search/funny%20cats
+    https://cryptic-castle-1280.herokuapp.com/search/tacos?offset=10
+    
+### View recent searches
+You may view this API's recent searches by calling the /latest endpoint
+
+    https://cryptic-castle-1280.herokuapp.com/latest
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var https = require("https");
+var mongo = require("mongodb").MongoClient;
+
 var app = express();
+var db;
 
 var port = process.env.PORT || 8080
 
@@ -8,11 +11,26 @@ var url = "https://www.googleapis.com/customsearch/v1"
             + "?key=AIzaSyAF3nvJIfuLTNbayac5a_S7CJI9AJLxGd0"
             + "&cx=010403160145310605403:9hfavwtzflc"
             + "&searchType=image";
+            
+mongo.connect("mongodb://imgsearch:qazwsxedc@ds047365.mongolab.com:47365/imgsearch", function(err, database){
+    if(err) throw err;
+    db = database;
+    
+    app.listen(port, function(){
+        console.log("App listening on port " + port);
+    });
+});
 
+
+app.use(express.static("public"));
 
 app.get("/search/:term", function(req, res){
     var googurl = url + "&q=" + req.params.term + (req.query.offset ? "&start=" +req.query.offset : "");
     console.log(googurl);
+    db.collection("searches").insertOne({term: req.params.term, when: new Date().toISOString()}, function(err, result){
+        if(err) throw err;
+        console.log("Search term stored");
+    });
     var resData = "";
     https.get(googurl, function(googres){
         googres.on("data", function(data){
@@ -20,14 +38,25 @@ app.get("/search/:term", function(req, res){
         }).on("end", function(){
             var resJSON = [];
             JSON.parse(resData).items.forEach(function(item){
-                resJSON.push({url: item.link, snippet: item.snippet, thumbnail: item.image.thumbnailLink, context: item.image.contextLink})
-            })
+                resJSON.push({url: item.link, snippet: item.snippet, thumbnail: item.image.thumbnailLink, context: item.image.contextLink});
+            });
             res.json(resJSON);
             res.end();
         });
     });
 });
 
-app.listen(port, function(){
-    console.log("App listening on port " + port);
+app.get("/latest", function(req, res){
+    var searches = [];
+   db.collection("searches").find().limit(10).each(function(err, doc){
+       if(err) throw err;
+       if(doc){
+           searches.push({term: doc.term, when: doc.when});
+           console.log({term: doc.term, when: doc.when});
+       } else {
+           res.json(searches);
+           res.end();
+       }
+   });
+
 });
